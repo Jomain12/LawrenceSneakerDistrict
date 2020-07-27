e320c8469566a775fd108924c3c39bfe31ac3226
Jomain Rouser <jomainrouser12@gmail.com>
2020-07-27 17:54:58 -0500

    created order model

--- amazona/backend/models/orderModel.js
+++ amazona/backend/models/orderModel.js
@@ -1,9 +1,48 @@
 import mongoose from "mongoose";
 
-const orderSchema = new mongoose.Schema({
-  itemsPrice: { type: Number },
-  taxPrice: { type: Number },
-  shippingPrice: { type: Number },
-  totalPrice: { type: Number },
-  isPaid: { type: Boolean },
+const shippingSchema = new mongoose.Schema({
+  address: { type: String, required: true },
+  city: { type: String, required: true },
+  postalCode: { type: Number, required: true },
+  country: { type: String, required: true },
 });
+
+const paymentSchema = new mongoose.Schema({
+  paymentMethod: { type: String, required: true },
+});
+
+const orderItemSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  qty: { type: Number, required: true },
+  image: { type: String, required: true },
+  price: { type: String, required: true },
+  product: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "product",
+    required: true,
+  },
+});
+
+const orderSchema = new mongoose.Schema(
+  {
+    user: { type: mongoose.Schema.Types, ref: "User", required: true },
+    orderItems: [orderItemSchema],
+    shipping: shippingSchema,
+    payment: paymentSchema,
+    itemsPrice: { type: Number },
+    taxPrice: { type: Number },
+    shippingPrice: { type: Number },
+    totalPrice: { type: Number },
+    isPaid: { type: Boolean, default: false },
+    paidAt: { type: Date },
+    isDelivered: { type: Boolean, default: false },
+    deliveredAt: { type: Date },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const orderModel = mongoose.model("Order", orderSchema);
+
+export default orderModel;
